85a1e09d466487e3973d196c7cbb8c1c2871b519
Fabio Jun <fabiojundev@gmail.com>
2022-05-05 23:07:26 -0300

    Add BST into two sum 2 solution

--- two-sum-2.js
+++ two-sum-2.js
@@ -10,12 +10,28 @@ var twoSum = function (nums, target) {
         const diff = target - nums[i];
 
         if (hashmap[diff] !== undefined) { //found
-            return [hashmap[diff] + 1, i + 1];
+            return [ i + 1, hashmap[diff] + 1];
         }
         else {
             hashmap[nums[i]] = i;
+            let left = 0;
+            let right = nums.length - 1;
+            let mid = 0;
+            while (left <= right) {
+                mid = ~~((left + right) / 2);
+                if (nums[mid] === diff && mid !== i) {
+                    return [i + 1, mid + 1];
+                }
+                hashmap[nums[mid]] = mid;
+                if (nums[mid] <= diff) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
         }
     }
 };
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], -2));
