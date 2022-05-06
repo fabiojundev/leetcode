82b1f2f80697b62adeb75e67dc6db9fd9b941459
Fabio Jun <fabiojundev@gmail.com>
2022-05-06 13:39:22 -0300

    Change twosum 2 solution using 2 pointers

--- two-sum-2.js
+++ two-sum-2.js
@@ -3,35 +3,30 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+ var twoSum = function(nums, target) {
     let hashmap = {}; // sub => index
+    
+    for( i = 0, j = nums.length -1; i < nums.length && i <= j ; i++, j-- ) {
+        const diff1 = target - nums[i];
+        const diff2 = target - nums[j];
 
-    for (i = 0; i < nums.length; i++) {
-        const diff = target - nums[i];
+        if( hashmap[diff1] !== undefined ) { //found
+           return  i < hashmap[diff1]
+                    ? [ i + 1, hashmap[diff1] + 1 ] 
+                    : [ hashmap[diff1] + 1, i + 1 ];
+        }
+        hashmap[nums[i]] = i;
 
-        if (hashmap[diff] !== undefined) { //found
-            return [ i + 1, hashmap[diff] + 1];
+        if( hashmap[diff2] !== undefined ) {
+            return j > hashmap[diff2] 
+                    ? [ hashmap[diff2] + 1, j + 1 ]
+                    : [ j + 1, hashmap[diff2] + 1 ];
         }
-        else {
-            hashmap[nums[i]] = i;
-            let left = 0;
-            let right = nums.length - 1;
-            let mid = 0;
-            while (left <= right) {
-                mid = ~~((left + right) / 2);
-                if (nums[mid] === diff && mid !== i) {
-                    return [i + 1, mid + 1];
-                }
-                hashmap[nums[mid]] = mid;
-                if (nums[mid] <= diff) {
-                    left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
-            }
-        }
+        hashmap[nums[j]] = j;
     }
 };
 
-console.log(twoSum([-1,-1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], -2));
+const nums = [5,25,75];
+const target = 100;
+
+console.log(twoSum(nums, target));
